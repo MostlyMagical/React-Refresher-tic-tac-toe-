315c408d67f1589761e965c144478babef923388
MostlyMagical <jb.mccann@outlook.com>
2022-01-01 21:42:35 +0000

    added turn and win functions +css

--- src/App.js
+++ src/App.js
@@ -2,6 +2,38 @@ import React from 'react';
 import './App.css';
 import './index.css'
 
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ]
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines [i]
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a]
+    }
+  }
+  return null
+}
+
+function calculateNextValue(squares) {
+  return squares.filter(Boolean).length % 2 === 0 ? 'X' : '0'
+}
+
+function calculateStatus(winner, squares, nextValue) {
+  return winner
+  ? `Winner: ${winner}`
+  : squares.every(Boolean)
+  ? `Scratch: Cat's game`
+  : `Next player: ${nextValue}`
+}
+
 function App() {
   return (
     <div className="App">
